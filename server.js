5151b29caecd79296b08eb2264d31389a0663b3f
yashua <yashuab.abraham@gmail.com>
2019-06-19 16:41:01 -0400

    put back accidently deleted code

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var path = require("path");
 var express = require("express");
-var mongoose = require("mongoose");git
+var mongoose = require("mongoose");
 var exphbs = require("express-handlebars");
 var app = express();
 
